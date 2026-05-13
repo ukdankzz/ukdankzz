cf55614c65556e40a4669f411d12de96ce0cf122
ukdankzz <ukdankzz@proton.me>
2026-05-13 10:40:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1087,7 +1087,7 @@ async def get_current_ltc_price():
 
 bot = Bot(token=API_TOKEN,
           default=DefaultBotProperties(parse_mode="Markdown"))
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
 from aiogram import types
 
